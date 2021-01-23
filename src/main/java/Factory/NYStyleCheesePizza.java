e40faa38c9d4973dc6e6fe7b06cf45efc00d1a18
AzdineElJattari <azdine.eljattari@hotmail.com>
2021-01-23 18:15:28 +0100

    Added Factory pattern + changes inside Factory pattern directory

---
+++ src/main/java/Factory/NYStyleCheesePizza.java
@@ -0,0 +1,14 @@
+package Factory;
+
+public class CheesePizza extends Pizza {
+
+    PizzaIngredientFactory ingredientFactory;
+
+    public CheesePizza(PizzaIngredientFactory ingredientFactory) {
+        this.ingredientFactory = ingredientFactory;
+    }
+
+    void prepare() {
+        dough = ingredientFactory.createDough();
+    }
+}
